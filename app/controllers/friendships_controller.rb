3166b7b11341ce43eb2f58654a89dc9ae9b54bab
vicmaburrito <vmaldaraca@gmail.com>
2021-07-09 18:19:13 -0500

    Add Friendship#create

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,3 +1,11 @@
 class FriendshipsController < ApplicationController
-    
+    def create 
+        @friendship = current_user.friendship.build(friend_id: params[:user_id])
+        @friendship.confirmed = false
+        if @friendship.save
+            flash[:notice] = 'Friend request was successfully sent.'
+            redirect_to request.referrer
+        else
+            redirect_to request.referrer, alert: @Friendship.errors.full_messages.join('. ').to_s
+    end
 end
