a3ad5d6c99b746478110154240c81b02ee94a048
vicmaburrito <vmaldaraca@gmail.com>
2021-07-10 12:42:04 -0500

    Create helpers and add missing helpers and partials

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -9,27 +9,30 @@ class FriendshipsController < ApplicationController
       redirect_to request.referrer, alert: @friendship.errors.full_messages.join('. ').to_s
     end
   end
+
   def update
     @friendship = Friendship.find(params[:id])
-    @friendship.confirmed = true 
+    @friendship.confirmed = true
     if @friendship.save
-        @friendship.confirm_friend
-        redirect_to request.referrer, notice: 'Friend request was succesfull'
-      else
-        redirect_to request.referrer, alert: @friendship.errors.full_messages.join('. ').to_s
-      end
+      @friendship.confirm_friend
+      redirect_to request.referrer, notice: 'Friend request was succesfull'
+    else
+      redirect_to request.referrer, alert: @friendship.errors.full_messages.join('. ').to_s
+    end
   end
+
   def index
     @friendship = current_user.friendships
     @inverse_friendships = current_user.inverse_friendships
   end
+
   def destroy
     @friendship = Friendship.find(friend_id: current_user.id, user_id: params[:user_id])
     if @friendship.destroy
-        @friendship.confirm_friend
-        redirect_to request.referrer, notice: 'Friend request declined'
-      else
-        redirect_to request.referrer, alert: @friendship.errors.full_messages.join('. ').to_s
-      end
+      @friendship.confirm_friend
+      redirect_to request.referrer, notice: 'Friend request declined'
+    else
+      redirect_to request.referrer, alert: @friendship.errors.full_messages.join('. ').to_s
+    end
   end
 end
