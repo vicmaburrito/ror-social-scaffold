47aa36ebae045181e3425b7351d5f584ebe048ef
vicmaburrito <vmaldaraca@gmail.com>
2021-07-12 12:24:19 -0500

    Add test for user2

--- spec/user_friendships_spec.rb
+++ spec/user_friendships_spec.rb
@@ -4,4 +4,19 @@ require 'spec_helper'
 RSpec.describe User, type: :feature do
 # User 1
 let(:user1) {User.create(name: 'Juan', email: 'user@example.com', password: 'admin123')}
+
+# User 2
+let(:user) { User.create(name: 'Diego', email: 'user@debian.com', password: 'admin321')}
+  describe '' do 
+    before do 
+      visit new_user_session_path
+      fill_in 'user[email]', with user1.email
+      fill_in 'user[password]', with user1.password
+      click_button 'Log in'
+    end 
+    it 'shows the right content for user index' do 
+      visit users_path(user2)
+      expect(page)-to have_button('Add Friend')
+    end
+  end 
 end
