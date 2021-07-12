2fe5eee44c3ecf18c23b240e706a3fe3193e6dd5
vicmaburrito <vmaldaraca@gmail.com>
2021-07-12 13:08:34 -0500

    Fix Rubocop

--- spec/user_friendships_spec.rb
+++ spec/user_friendships_spec.rb
@@ -2,21 +2,21 @@ require 'rails_helper'
 require 'spec_helper'
 
 RSpec.describe User, type: :feature do
-# User 1
-let(:user1) {User.create(name: 'Juan', email: 'user@example.com', password: 'admin123')}
+  # User 1
+  let(:user1) { User.create(name: 'Juan', email: 'user@example.com', password: 'admin123') }
 
-# User 2
-let(:user) { User.create(name: 'Diego', email: 'user@debian.com', password: 'admin321')}
-  describe 'friendship' do 
-    before do 
+  # User 2
+  let(:user2) { User.create(name: 'Diego', email: 'user@debian.com', password: 'admin321') }
+  describe 'friendship' do
+    before do
       visit new_user_session_path
-      fill_in 'user[email]', with user1.email
-      fill_in 'user[password]', with user1.password
+      fill_in 'user[email]', with: user1.email
+      fill_in 'user[password]', with: user1.password
       click_button 'Log in'
-    end 
-    it 'shows the right content for user index' do 
+    end
+    it 'shows the right content for user index' do
       visit users_path(user2)
       expect(page).to have_button('Add Friend')
     end
-  end 
+  end
 end
