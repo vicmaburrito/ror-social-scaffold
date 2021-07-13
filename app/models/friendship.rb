d32797abd32d4817417772ef9dea15644d44e0ed
vicmaburrito <vmaldaraca@gmail.com>
2021-07-13 14:54:10 -0500

    Prevent double friendship records

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,3 +1,17 @@
+class DupplicateValidator < ActiveModel::Validator
+  def validate(record)
+    friend1 = Friendship.find_by(user_id: record.user_id, friend_id: record.friend_id)
+    friend2 = Friendship.find_by(friend_id: record.user_id, user_id: record.friend_id)
+    record.errors[:base].push('This relationship is already created') unless friend1.nil? && friend2.nil?
+  end
+end
+
+class SelfFriendshipValidator < ActiveModel::Validator
+  def validate(record)
+    record.errors[:base].push("You can't be friends with yourself") if record.user_id == record.friend_id
+  end
+end
+
 class Friendship < ApplicationRecord
   include ActiveModel::Validations
   # validates_with DupplicateValidator, on: :create
