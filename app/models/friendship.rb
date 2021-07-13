9189644ac0476f979c933a4a9a2e7b7592c72b82
vicmaburrito <vmaldaraca@gmail.com>
2021-07-13 14:51:48 -0500

    Add validations to friendship

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,18 @@
 class Friendship < ApplicationRecord
+  include ActiveModel::Validations
+  # validates_with DupplicateValidator, on: :create
+  validates_with SelfFriendshipValidator
+
   belongs_to :user
   belongs_to :friend, class_name: 'User'
+
+  validates :user, presence: true
+  validates :friend, presence: true
+
+  def confirm_friend
+    # self.update_attributes(confirmed: true)
+    Friendship.create!(friend_id: user_id,
+                       user_id: friend_id,
+                       confirmed: true)
+  end
 end
