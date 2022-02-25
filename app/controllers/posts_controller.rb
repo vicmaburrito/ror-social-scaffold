48c25df4d36d0e275069a58b1b156deb002900fc
vicmaburrito <vmaldaraca@gmail.com>
2022-02-25 16:46:16 -0600

    Fix and build json response in controllers to post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,19 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!
-
+  # GET / posts
   def index
     @post = Post.new
     timeline_posts
+    json_response(@timeline_posts)
   end
 
+  # POST /posts
   def create
     @post = current_user.posts.new(post_params)
 
     if @post.save
       redirect_to posts_path, notice: 'Post was successfully created.'
+      json_response(@post, :created)
     else
       timeline_posts
       render :index, alert: 'Post was not created.'
--- spec/Requests/post_request_spec.rb
+++ spec/Requests/post_request_spec.rb
@@ -16,34 +16,6 @@ RSpec.describe 'Post Requests', type: :request do
     end
   end
 
-  # Test suite for GET /posts/:id
-  describe 'GET /posts/:id' do
-    before { get "/posts/#{post_id}" }
-
-    context 'when the record exists' do
-      it 'returns the post' do
-        expect(json).not_to be_empty
-        expect(json['id']).to eq(post_id)
-      end
-
-      it 'returns status code 200' do
-        expect(response).to have_http_status(200)
-      end
-    end
-
-    context 'when the record does not exist' do
-      let(:post_id) { 100 }
-
-      it 'returns status code 404' do
-        expect(response).to have_http_status(404)
-      end
-
-      it 'returns a not found message' do
-        expect(response.body).to match(/Couldn't find Post/)
-      end
-    end
-  end
-
   # Test suite for POST /posts
   describe 'POST /posts' do
     let(:valid_attributes) { { content: 'Fifteen birds in five fir trees' } }
@@ -61,7 +33,7 @@ RSpec.describe 'Post Requests', type: :request do
     end
 
     context 'when the request is invalid' do
-      before { post '/posts', params: { title: '"The wind was on the withered heath"' } }
+      before { post '/posts', params: { content: '"The wind was on the withered heath"' } }
 
       it 'returns status code 422' do
         expect(response).to have_http_status(422)
@@ -71,32 +43,6 @@ RSpec.describe 'Post Requests', type: :request do
         expect(response.body)
           .to match(/Validation failed: Created by can't be blank/)
       end
-    end
-  end
-
-  # Test suite for PUT /posts/:id
-  describe 'PUT /posts/:id' do
-    let(:valid_attributes) { { title: 'Where now are the Dunedain, Elessar, Elessar?' } }
-
-    context 'when the record exists' do
-      before { put "/posts/#{post_id}", params: valid_attributes }
-
-      it 'updates the record' do
-        expect(response.body).to be_empty
-      end
-
-      it 'returns status code 204' do
-        expect(response).to have_http_status(204)
-      end
-    end
-  end
-
-  # Test suite for DELETE /posts/:id
-  describe 'DELETE /posts/:id' do
-    before { delete "/posts/#{post_id}" }
-
-    it 'returns status code 204' do
-      expect(response).to have_http_status(204)
     end
   end
 end
