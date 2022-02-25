d399aa973ba50472a1da25d354351f77e68c49f3
vicmaburrito <vmaldaraca@gmail.com>
2022-02-25 16:06:00 -0600

    Add test suite for GET posts :id

--- spec/Requests/post_request_spec.rb
+++ spec/Requests/post_request_spec.rb
@@ -7,13 +7,40 @@ RSpec.describe 'Post Requests', type: :request do
     before { get '/posts' }
 
     it 'returns posts' do
-      # TODO: build `json` as custom helper to parse JSON responses
-    #   expect(json).not_to be_empty
-    #   expect(json.size).to eq(size.to_num) #Set size
+      # DONE: build `json` as custom helper to parse JSON responses
+      expect(json).not_to be_empty
     end
 
     it 'returns status code 200' do
       expect(response).to have_http_status(200)
+    end
+  end
+
+  # Test suite for GET /posts/:id
+  describe 'GET /posts/:id' do
+    before { get "/posts/#{post_id}" }
+
+    context 'when the record exists' do
+      it 'returns the post' do
+        expect(json).not_to be_empty
+        expect(json['id']).to eq(post_id)
+      end
+
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context 'when the record does not exist' do
+      let(:post_id) { 100 }
+
+      it 'returns status code 404' do
+        expect(response).to have_http_status(404)
+      end
+
+      it 'returns a not found message' do
+        expect(response.body).to match(/Couldn't find Post/)
+      end
     end
   end
 end
